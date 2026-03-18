7b197892999e54f706b11ee8b4f50accce3b4abc
Claire Hassig <processblueproductions@gmail.com>
2026-03-18 12:00:01 -0500

    chore: fix wordle submodule

---
+++ wordle/src/utils.js
@@ -0,0 +1,32 @@
+// Validate guess length and letters only
+// TODO: Make alert not an alert
+export function validateGuess(guess, letters)
+{
+    if (guess.length == letters && /^[A-Za-z]+$/.test(guess)) // Regex via geeksforgeeks.org
+        return true
+    else if (guess.length != letters)
+        alert('Guess must be exactly 5 letters');
+    else 
+        alert('Guess must be letters only');
+    return false
+}
+
+// R = right letter, right place
+// W = right letter, wrong place
+// X = not in word
+export function checkGuess(guess, answer) // TODO: Finish this
+{
+    var result = []
+    guess = guess.toUpperCase();
+    // For each letter, compare against answer
+    for (var i = 0; i < answer.length; i++)
+        if (guess[i] == answer[i])
+            result[i] = 'R'; // right letter, right place
+        else if (answer.includes(guess[i]))
+            result[i] = 'W'; // right letter, wrong place
+        else 
+            // Add to used letters
+            result[i] = 'X'; // wrong letter
+    console.log('result = ' + result);
+    return result;    
+}
