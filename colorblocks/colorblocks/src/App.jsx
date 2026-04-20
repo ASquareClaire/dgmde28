6f002f21f1a8e2a4c3a3affb73caa0a520d510ae
Claire Hassig <processblueproductions@gmail.com>
2026-04-20 16:42:52 -0400

    boxes and colors work

--- colorblocks/colorblocks/src/App.jsx
+++ colorblocks/colorblocks/src/App.jsx
@@ -7,10 +7,18 @@ import './App.css'
 // TODO: Make box component
 function Box(number)
 {
+  const colorMap = 
+  {
+    7: 'black-box', 8: 'blue-box', 9: 'red-box',
+    4: 'gold-box',  5: 'black-box', 6: 'blue-box',
+    1: 'red-box',   2: 'gold-box',  3: 'black-box',
+  };
+  var color = colorMap[number];
+  //const colorClasses = ['black-box', 'blue-box', 'red-box', 'gold-box'];
   return <div 
             id={number} 
             key={number}
-            className={'small-box'}
+            className={`small-box ${color}`}
         >
             {number}
         </div>
@@ -44,7 +52,7 @@ function BigBox()
   const rows = 3;
   const bigBox = []
   // Add rows
-  var startingNum = 1
+  var startingNum = 1;
   for (var i = 0; i < rows; i++)
   {
     // Add boxes x3 (assign numbers)
@@ -53,8 +61,8 @@ function BigBox()
   }
 
   // TODO: Assign colors (black, blue, red, gold)
-  var colorClasses = ['black-box', 'blue-box', 'red-box', 'gold-box'];
-  return <div>{bigBox}</div>
+  //var colorClasses = ['black-box', 'blue-box', 'red-box', 'gold-box'];
+  return <div className='big-box'>{bigBox}</div>
 }
 
 
