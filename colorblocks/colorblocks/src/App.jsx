d3cd3dd231d132d8980d6e9de8afdad1a9e2c128
Claire Hassig <processblueproductions@gmail.com>
2026-04-20 16:18:56 -0400

    basic number boxes and rows working

--- colorblocks/colorblocks/src/App.jsx
+++ colorblocks/colorblocks/src/App.jsx
@@ -7,19 +7,35 @@ import './App.css'
 // TODO: Make box component
 function Box(number)
 {
-  return <div id={number} className={'smallBox'}></div>
+  return <div 
+            id={number} 
+            key={number}
+            className={'small-box'}
+        >
+            {number}
+        </div>
 }
 
 // TODO: Make row component
-function Row(boxes)
+function Row(boxes, rowNum, startingNum)
 {
   var row = [];
 
   // Add boxes to row
   for (var i = 0; i < boxes; i++)
-    row.push(Box(i))
+  {
+    row.push(Box(startingNum));
+    console.log('startingNum: ' + startingNum)
+    startingNum++;
+  }
 
-  return <div id={'row'}>{row}</div>
+  return <div 
+            id={`row${rowNum}`}
+            key={`row${rowNum}`}
+            className={'row'}
+        >
+            {row}
+        </div>
 }
 
 // TODO: Make box component
@@ -28,12 +44,16 @@ function BigBox()
   const rows = 3;
   const bigBox = []
   // Add rows
+  var startingNum = 1
   for (var i = 0; i < rows; i++)
+  {
     // Add boxes x3 (assign numbers)
-    bigBox.push(Row(rows));
+    bigBox[rows - 1 - i] = (Row(rows, i, startingNum));
+    startingNum += 3;
+  }
 
   // TODO: Assign colors (black, blue, red, gold)
-  var colors = ['black', 'blue', 'red', 'gold'];
+  var colorClasses = ['black-box', 'blue-box', 'red-box', 'gold-box'];
   return <div>{bigBox}</div>
 }
 
