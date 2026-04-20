6a1f83c59da66150ad7818dd81f77d5ab8273a37
Claire Hassig <processblueproductions@gmail.com>
2026-04-20 15:44:35 -0400

    skeleton todo list

--- colorblocks/colorblocks/src/App.jsx
+++ colorblocks/colorblocks/src/App.jsx
@@ -4,12 +4,36 @@
 // import heroImg from './assets/hero.png'
 import './App.css'
 
+// TODO: Make box component
+function Box()
+{
+
+}
+
+// TODO: Make row component
+function Row()
+{
+  
+}
+
+// TODO: Make box component
+function BigBox()
+{
+  const bigBox = []
+  // TODO: Add rows x3
+    // TODO: Add boxes x3 (assign numbers)
+  // TODO: Assign colors (black, blue, red, gold)
+  return <div>{bigBox}</div>
+}
+
+
+// Main
 function App() {
- // const [count, setCount] = useState(0)
+//const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <BigBox />
     </>
   )
 }
