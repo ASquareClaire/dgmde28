34a488934f53d2b946dced30e3f8fdfdf8f7fcb4
Claire Hassig <processblueproductions@gmail.com>
2026-04-21 15:08:24 -0400

    cleaned up, commented

--- colorblocks/src/App.jsx
+++ colorblocks/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
 
-// TODO: Make box component
+// Box Component
 function Box({number})
 {
+  // Assign background color
   const colorMap = 
   {
     7: 'black', 8: 'blue', 9: 'red',
@@ -10,9 +11,10 @@ function Box({number})
     1: 'red',   2: 'gold',  3: 'black',
   };
   var color = colorMap[number];
+
+  // Return Box
   return <div 
             id={number} 
-            //key={number}
             className={`small-box ${color}`}
             onClick={() => alert(number)}
         >
@@ -20,7 +22,7 @@ function Box({number})
         </div>
 }
 
-// TODO: Make row component
+// Row Component
 function Row({boxes, rowNum, startingNum})
 {
   var row = [];
@@ -29,24 +31,24 @@ function Row({boxes, rowNum, startingNum})
   for (var i = 0; i < boxes; i++)
   {
     row.push(<Box key={startingNum} number={startingNum} />)
-    console.log('startingNum: ' + startingNum)
     startingNum++;
   }
 
+  // Return Row
   return <div 
             id={`row${rowNum}`}
-            //key={`row${rowNum}`}
             className={'row'}
         >
             {row}
         </div>
 }
 
-// TODO: Make box component
+// BigBox Component
 function BigBox()
 {
   const rows = 3;
   const bigBox = []
+
   // Add rows
   var startingNum = 1;
   for (var i = 0; i < rows; i++)
@@ -56,21 +58,13 @@ function BigBox()
     startingNum += 3;
   }
 
-  // TODO: Assign colors (black, blue, red, gold)
-  //var colorClasses = ['black-box', 'blue-box', 'red-box', 'gold-box'];
   return <div className='big-box'>{bigBox}</div>
 }
 
 
 // Main
 function App() {
-//const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <BigBox />
-    </>
-  )
+  return <BigBox />
 }
 
 export default App
